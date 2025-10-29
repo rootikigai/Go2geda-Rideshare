6381e63023e095835c5588b16b7ffc640bfa8a57
rootikigai <ifykel360@gmail.com>
2025-10-29 23:57:00 +0100

    Global configuration across the app for the tailwind file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,17 @@
-/* @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        go2geda: {
+          light: "#c7f9cc", // light pastel green
+          DEFAULT: "#80ed99", // main green
+          dark: "#38a169", // darker shade for text or accents
+        },
+        danger: "#e63946", // red for logout or warnings
+      },
+    },
   },
   plugins: [],
-}
-
+};
