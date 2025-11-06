c0a25f438321b3f313ba57a77e253de546f7fa29
rootikigai <ifykel360@gmail.com>
2025-11-06 21:25:17 +0100

    Add Create ride and complete ride functions

---
+++ backend/controllers/rideController.ts
@@ -0,0 +1,69 @@
+import type { Request, Response } from 'express'
+import { PrismaClient, RideStatus } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const getUserRides = async (req: Request, res: Response) => {
+    const user = (req as any).user;
+
+    try {
+        let rides;
+        if(user.role === 'PASSENGER'){
+            rides = await prisma.ride.findMany({
+                where: { passengerId: user.userId }
+            });
+        }
+        // else if (user.role === 'DRIVER') {
+        //     rides = await prisma.ride.findMany({
+        //         where: { driverId: user.userId }
+        //     })
+        // }
+    } catch (error) {
+        
+    }
+}
+
+
+export const completeRide = async (req: Request, res: Response) => {
+    const { rideId } = req.params;
+    const user = (req as any).user;
+
+    if (user.role !== 'DRIVER') {
+        return res.status(403).json({ message: 'Only drivers can complete rides' });
+    }
+
+    try {
+        const rideIdNum = parseInt(rideId as string,10);
+        const ride = await prisma.ride.update({
+            where: { id: rideIdNum },
+            data: { status: RideStatus.COMPLETED }
+        });
+        res.status(200).json({ message: 'Ride marked as completed', ride });
+    } catch (error) {
+        res.status(500).json({ message: 'Error completing ride', error });
+    }
+};
+
+export const createRide = async (req: Request, res: Response) => {
+    const user = (req as any).user;
+
+    if (user.role !== 'PASSENGER') {
+        return res.status(403).json({message: 'Only passengers can request rides'})
+    }
+    const { origin, destination } = req.body;
+
+    try {
+        const ride = await prisma.ride.create({
+            data: {
+                origin,
+                destination,
+                passengerId: user.userId,
+                requestStatus: 'PENDING',
+                status: 'SCHEDULED'
+            }
+        })
+        res.status(201).json({ message: 'Ride created successfully', ride });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating ride', error });
+    }
+};
