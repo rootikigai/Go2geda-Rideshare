29c2d8e86defd142cef0891660c73dcf979e428b
rootikigai <ifykel360@gmail.com>
2025-11-19 00:16:48 +0100

    Fix typo in authentication middleware import and update routes to use correct function

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { signup, login, getUserProfile } from '../controllers/userController.ts';
-import { ortentikate } from '../middleware/auth.ts';
+import { authenticate } from '../middleware/auth.ts';
 import { authorizeRoles } from '../middleware/roleMiddleware.ts';
 
 const router = Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.get('/admin/dashboard', ortentikate, authorizeRoles('ADMIN'), (req, res) => {
+router.get('/admin/dashboard', authenticate, authorizeRoles('ADMIN'), (req, res) => {
     res.json({ message: "Admin Dashboard - Welcome!" })
 })
-router.get('/:id', ortentikate, getUserProfile);
+router.get('/:id', authenticate, getUserProfile);
 
 export default router;
